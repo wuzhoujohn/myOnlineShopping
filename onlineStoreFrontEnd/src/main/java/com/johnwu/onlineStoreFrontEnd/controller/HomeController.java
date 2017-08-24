2ffa2c5731ecf0682ce77f2da58ec2d89c2d6b44
wuzhoujohn <wuzhoujohn@hotmail.com>
2017-08-23 21:57:06 -0400

    Successfully loading the static resources and modified the navibar jsp file and added more jsp files

--- onlineStoreFrontEnd/src/main/java/com/johnwu/onlineStoreFrontEnd/controller/HomeController.java
+++ onlineStoreFrontEnd/src/main/java/com/johnwu/onlineStoreFrontEnd/controller/HomeController.java
@@ -9,10 +9,27 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value ={ "/", "/home", "/index"}, method = RequestMethod.GET)
 	public ModelAndView home() {
-		//System.out.println("we are in admissionform ");
+		System.out.println("we are in home handlerMethod");
 		ModelAndView model = new ModelAndView("home");
+		model.addObject("title", "Zhou Wu");
+		model.addObject("userClickHome", true);
 		return model;
 	}
+	
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
+	public ModelAndView about() {
+		ModelAndView model = new ModelAndView("about");
+		model.addObject("userClickAbout", true);
+		return model;
+	}
+	
+	@RequestMapping(value = "/contact", method = RequestMethod.GET)
+	public ModelAndView contact() {
+		ModelAndView model = new ModelAndView("contact");
+		model.addObject("userClickContact", true);
+		return model;
+	}
+	
 }
